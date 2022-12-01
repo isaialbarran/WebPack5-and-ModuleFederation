1114f872678a41aa39a09e574f66e9e4d7af0724
Isai <isai@Isais-MacBook-Pro.local>
2022-12-01 13:34:17 +0100

    useContext with store working

--- container/src/App.jsx
+++ container/src/App.jsx
@@ -1,51 +1,58 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Route, Routes, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home";
+
+import { useCount } from "container/store";
 
 import "./index.css";
 
 const Feasibility = React.lazy(() => import("feasibility/Counter"));
 const Activation = React.lazy(() => import("activation/Box"));
 
-const App = () => (
-  <BrowserRouter>
-    <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      <div className="my-10">
-        <div className="list">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/feasibility">Feasibility</Link>
-            </li>
-            <li>
-              <Link to="/activation">Activation</Link>
-            </li>
-          </ul>
+const App = () => {
+  const [count, setCount] = useCount();
+  return (
+    <BrowserRouter>
+      <div>Count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <div className="mt-10 text-3xl mx-auto max-w-6xl">
+        <div className="my-10">
+          <div className="list">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/feasibility">Feasibility</Link>
+              </li>
+              <li>
+                <Link to="/activation">Activation</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/feasibility"
+              element={
+                <Suspense fallback={"loading..."}>
+                  <Feasibility />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/activation"
+              element={
+                <Suspense fallback={"loading..."}>
+                  <Activation />
+                </Suspense>
+              }
+            />
+          </Routes>
         </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/feasibility"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Feasibility />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/activation"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Activation />
-              </Suspense>
-            }
-          />
-        </Routes>
       </div>
-    </div>
-  </BrowserRouter>
-);
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- container/src/bootstrap.jsx
+++ container/src/bootstrap.jsx
@@ -1,8 +1,13 @@
-import App from './App';
-import React from 'react';
+import App from "./App";
+import React from "react";
 
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import { CountProvider } from "container/store";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <CountProvider>
+    <App />
+  </CountProvider>
+);
